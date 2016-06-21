54d4e7474a7a543452f6fd2c415c48b4059968e5
Justin <jrsnow8921@gmail.com>
2016-06-21 12:17:14 -0400

    build

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'SnowNasaPython',
   packages = ['SnowNasaPython'], 
-  version = '0.1',
+  version = '1.0',
   description = 'A python lib to interact with Nasas API',
   author = 'Justin Snow',
   author_email = 'jrsnow8921@gmail.com',
   url = 'https://github.com/Jrsnow8921/SnowNasaPython.git',
-  download_url = 'https://github.com/Jrsnow8921/SnowNasaPython/archive/0.1.tar.gz', 
+  download_url = 'https://github.com/Jrsnow8921/SnowNasaPython/archive/1.0.tar.gz', 
   keywords = ['Nasa', 'API'], 
   classifiers = [],
 )
